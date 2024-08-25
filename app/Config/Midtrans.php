aa240182151663d97ef5712d29390d73e33e997b
Ayub Rantesalu <105459634+aybs13@users.noreply.github.com>
2024-08-26 00:39:19 +0800

    Update Midtrans.php

--- app/Config/Midtrans.php
+++ app/Config/Midtrans.php
@@ -4,8 +4,8 @@ namespace Config;
 
 class Midtrans
 {
-    public $serverKey = 'SB-Mid-server-PT8RpIyNjtmfxJzBUqt2_Kuv';
-    public $clientKey = 'SB-Mid-client-erFAq6u8gciJrbGt';
+    public $serverKey = 'midtrans server key';
+    public $clientKey = 'midtrans client key';
     public $isProduction = false; // Set to true for production
     public $isSanitized = true;
     public $is3ds = true;
